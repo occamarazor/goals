9d11b6152bcbbe5eacda79c26e09beedb71d543c
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-07 16:38:17 +0300

    Server: goals controller with mongoose

--- src/server/controllers/goals.js
+++ src/server/controllers/goals.js
@@ -1,10 +1,27 @@
 import asyncHandler from 'express-async-handler';
+import GoalModel from '../models/goal.js';
 
 // @desc Get goals
 // @route GET /api/goals/
 // @access Private
 export const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get goals' });
+  const goals = await GoalModel.find();
+  res.status(200).json(goals);
+});
+
+// @desc Delete goals
+// @route DELETE /api/goals/
+// @access Private
+export const deleteGoals = asyncHandler(async (req, res) => {
+  const goals = await GoalModel.find();
+
+  if (goals.length) {
+    await GoalModel.deleteMany();
+    res.status(200).json({ message: `All goals deleted`, data: goals });
+  } else {
+    res.status(400);
+    throw new Error('No goals available');
+  }
 });
 
 // @desc Create goal
@@ -14,25 +31,46 @@ export const createGoal = asyncHandler(async (req, res) => {
   const { text, duration } = req.body;
 
   if (text && duration) {
-    res.status(201).json({ message: 'Create goal' });
+    const goal = await GoalModel.create({ text, duration });
+    res.status(201).json({ message: `Goal with ID: ${goal.id} created`, data: goal });
   } else {
     res.status(400);
     throw new Error('Please include both text & duration');
   }
 });
 
-// TODO: error handling
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
 export const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal: ${req.params.id}` });
+  const foundGoal = await GoalModel.findById(req.params.id);
+
+  if (foundGoal) {
+    const { text, duration } = req.body;
+    const updatedGoal = await GoalModel.findByIdAndUpdate(
+      req.params.id,
+      { text, duration },
+      { new: true },
+    );
+
+    res.status(200).json({ message: `Goal with ID: ${req.params.id} updated`, data: updatedGoal });
+  } else {
+    res.status(400);
+    throw new Error(`No goal with ID: ${req.params.id}`);
+  }
 });
 
-// TODO: error handling
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
 export const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal: ${req.params.id}` });
+  const foundGoal = await GoalModel.findById(req.params.id);
+
+  if (foundGoal) {
+    await foundGoal.remove();
+    res.status(200).json({ message: `Goal with ID: ${req.params.id} deleted`, data: foundGoal });
+  } else {
+    res.status(400);
+    throw new Error(`No goal with ID: ${req.params.id}`);
+  }
 });
--- src/server/routes/goals.js
+++ src/server/routes/goals.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getGoals, createGoal, updateGoal, deleteGoal } from '../controllers/goals.js';
+import { getGoals, deleteGoals, createGoal, updateGoal, deleteGoal } from '../controllers/goals.js';
 
 const router = Router();
 
-router.route('/').get(getGoals).post(createGoal);
+router.route('/').get(getGoals).delete(deleteGoals).post(createGoal);
 
 router.route('/:id').put(updateGoal).delete(deleteGoal);
 
