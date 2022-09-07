54201255ccd0b6a5119b925f64abf8b33c7afc04
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-07 12:03:54 +0300

    Server: goals API

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,11 @@
 import { config } from 'dotenv';
 import express from 'express';
+import router from './routes/goals.js';
 
 config();
 const PORT = process.env.port || 5000;
 const app = express();
+
+app.use('/api/goals', router);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/server/routes/goals.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Get goals' });
+});
+
+router.post('/', (req, res) => {
+  res.status(201).json({ message: 'Create goal' });
+});
+
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Update goal: ${req.params.id}` });
+});
+
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `Delete goal: ${req.params.id}` });
+});
+
+export default router;
