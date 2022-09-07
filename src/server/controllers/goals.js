ee4c643608fd65551e7ca7fc6a568af413f6ebd8
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-07 13:09:48 +0300

    Server: express async handler

--- src/server/controllers/goals.js
+++ src/server/controllers/goals.js
@@ -1,14 +1,16 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc Get goals
 // @route GET /api/goals/
 // @access Private
-export const getGoals = (req, res) => {
+export const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get goals' });
-};
+});
 
 // @desc Create goal
 // @route POST /api/goals/
 // @access Private
-export const createGoal = (req, res) => {
+export const createGoal = asyncHandler(async (req, res) => {
   const { text, duration } = req.body;
 
   if (text && duration) {
@@ -17,20 +19,20 @@ export const createGoal = (req, res) => {
     res.status(400);
     throw new Error('Please include both text & duration');
   }
-};
+});
 
 // TODO: error handling
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
-export const updateGoal = (req, res) => {
+export const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal: ${req.params.id}` });
-};
+});
 
 // TODO: error handling
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
-export const deleteGoal = (req, res) => {
+export const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal: ${req.params.id}` });
-};
+});
