f21367e25bacb223d1e091ad38c97b11802a76bd
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-08 12:03:00 +0300

    Server: port name fix

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ import connectDB from './configs/db.js';
 config();
 connectDB();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(json());
