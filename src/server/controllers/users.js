070bd2d506f6d6d69396fbb86dcd44cf226894f8
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-13 12:58:03 +0300

    Server: create new user

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -1,10 +1,35 @@
 import asyncHandler from 'express-async-handler';
+import bcryptjs from 'bcryptjs';
+import UserModel from '../models/user.js';
 
 // @desc Create user
 // @route POST /api/users/
 // @access Public
 export const createUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Create User' });
+  const { name, email, password } = req.body;
+
+  if (name && email && password) {
+    const userExists = await UserModel.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error(`User with email: ${email} already exists`);
+    } else {
+      // Hash password
+      const passwordSalt = await bcryptjs.genSalt(10);
+      const passwordHashed = await bcryptjs.hash(password, passwordSalt);
+
+      // Create user
+      const user = await UserModel.create({ name, email, password: passwordHashed });
+      res.status(201).json({
+        message: `User with ID: ${user.id} created`,
+        data: user,
+      });
+    }
+  } else {
+    res.status(400);
+    throw new Error('Please include name, email & password');
+  }
 });
 
 // @desc Authenticate user
