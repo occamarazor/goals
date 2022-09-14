24225eb759d01e78e7d8755af6af9b3857b3562c
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-14 16:42:47 +0300

    Server: non-existant user fix

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -7,13 +7,18 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       const [, token] = req.headers.authorization.split(' ');
       const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await UserModel.findById(userId).select('-password');
 
-      req.user = await UserModel.findById(userId).select('-password');
+      if (user) {
+        req.user = user;
+      } else {
+        throw new Error(`User with ID: ${userId} does not exist`);
+      }
       next();
     } catch (e) {
       console.log(e);
       res.status(401);
-      throw new Error('Not authorized');
+      throw new Error('Not authorized: invalid token');
     }
   } else {
     res.status(401);
