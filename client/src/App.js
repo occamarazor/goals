ad3c4c80f01eac2a0c54bf871a3415a905d90849
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-15 13:02:38 +0300

    Client: added tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,8 @@
-const App = () => <h1>Test</h1>;
+const App = () => (
+  <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10'>
+    <p className='text-3xl text-gray-700 font-bold mb-5'>Tailwind test</p>
+    <p className='text-gray-500 text-lg'>test test test</p>
+  </div>
+);
 
 export default App;
