3cf14c868b1a16e92faae7d9c684a44e864dd06b
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-20 12:01:41 +0300

    Client: todos list

--- client/src/features/application/App.js
+++ client/src/features/application/App.js
@@ -1,6 +1,13 @@
 import AppHeader from 'features/application/AppHeader';
 import AppMain from 'features/application/AppMain';
 
+// TODO:
+//  0. Error boundaries
+//  1. Authentication
+//  2. Notifications
+//  3. Form validation
+//  4. Unit testing
+//  5. Animation
 const App = () => (
   <>
     <AppHeader />
