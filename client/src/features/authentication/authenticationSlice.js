737f64f28ec71f3d149efa39a3b666556922a544
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-21 16:21:10 +0300

    Client: login slice

---
+++ client/src/features/authentication/authenticationSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import REQUEST_STATUS_TYPES from 'features/common/commonConstants';
+
+const initialState = {
+  status: REQUEST_STATUS_TYPES.INITIAL,
+  token: false,
+  error: '',
+};
+
+const authenticationSlice = createSlice({
+  name: 'authentication',
+  initialState,
+  reducers: {
+    submitLogin: (state) => {
+      state.status = REQUEST_STATUS_TYPES.LOADING;
+    },
+    submitLoginSuccess: (state, { payload }) => {
+      state.token = payload;
+      state.status = REQUEST_STATUS_TYPES.SUCCESS;
+    },
+    submitLoginError: (state, { payload }) => {
+      state.error = payload;
+      state.status = REQUEST_STATUS_TYPES.ERROR;
+    },
+  },
+});
+
+export const { submitLogin, submitLoginSuccess, submitLoginError } = authenticationSlice.actions;
+
+export const selectAuthentication = (state) => state.authentication;
+
+export default authenticationSlice.reducer;
--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import counterReducer from 'features/counter/counterSlice';
 import navigationSlice from 'features/navigation/navigationSlice';
+import authenticationSlice from 'features/authentication/authenticationSlice';
 
 export default combineReducers({
   navigation: navigationSlice,
+  authentication: authenticationSlice,
   counter: counterReducer,
 });
