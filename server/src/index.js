28e4a04ee2c8ae26bf2642985a96d83290585b73
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-21 18:06:55 +0300

    Server: cors middleware, dev env vars

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import { config } from 'dotenv';
 import express, { json, urlencoded } from 'express';
+import cors from 'cors';
 import connectDB from './configs/db.js';
 import errorHandler from './middleware/error.js';
 import usersRoutes from './routes/users.js';
@@ -11,6 +12,11 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use('/api/users', usersRoutes);
