d3f8b796e2837326019abee7393b3111fa88600f
occamarazor <30173929+occamarazor@users.noreply.github.com>
2022-09-22 17:37:22 +0300

    Client: no auth redirect

--- client/src/features/application/AppMain.js
+++ client/src/features/application/AppMain.js
@@ -1,12 +1,22 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useRoutes } from 'react-router-dom';
+import { LOGIN } from 'configs/routing/routingPaths';
 import { publicRoutes, privateRoutes } from 'configs/routing/routingRoutes';
+import { useNavigationHandler } from 'features/navigation/navigationHooks';
 import { selectAuthentication } from 'features/authentication/authenticationSlice';
 
 const AppMain = () => {
   const { user } = useSelector(selectAuthentication);
   const routes = user ? privateRoutes : publicRoutes;
   const element = useRoutes(routes);
+
+  const handleLoginNavigate = useNavigationHandler(LOGIN);
+  useEffect(() => {
+    if (!user) {
+      handleLoginNavigate();
+    }
+  }, [user, handleLoginNavigate]);
 
   return <main>{element}</main>;
 };
